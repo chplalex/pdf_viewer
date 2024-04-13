35cd43ccaf33811fb052248823914e99d70d99a8
Umesh <umesh.chakradhar@8squarei.com>
2024-04-13 14:29:05 +0545

    fix : zoom added in pdf list

--- lib/src/list_viewer.dart
+++ lib/src/list_viewer.dart
@@ -3,19 +3,26 @@ import 'package:flutter/material.dart';
 
 class PDFListViewer extends StatefulWidget {
   final PDFDocument document;
-  List<Image?>? _images;
   final bool preload;
-  double? _loadingPageHeight;
+  final double? loadingPageHeight;
+  final Function(double)? onZoomChanged;
+  final int? zoomSteps;
+  final double? minScale;
+  final double? maxScale;
+  final double? panLimit;
 
   /// Shows PDF of multiple pages in list mode
   PDFListViewer({
     Key? key,
     required this.document,
     this.preload = false,
-    double loadingPageHeight = 400,
-  }) : super(key: key) {
-    _loadingPageHeight = loadingPageHeight;
-  }
+    this.onZoomChanged,
+    this.loadingPageHeight = 400,
+    this.zoomSteps = 3,
+    this.minScale = 1.0,
+    this.maxScale = 5.0,
+    this.panLimit = 1.0,
+  }) : super(key: key);
 
   @override
   _PDFListViewerState createState() => _PDFListViewerState();
@@ -23,16 +30,16 @@ class PDFListViewer extends StatefulWidget {
 
 class _PDFListViewerState extends State<PDFListViewer> {
   bool _preloaded = false;
+  List<Image?>? _images;
 
   @override
   void initState() {
     super.initState();
-    widget._images =
-        List<Image?>.filled(widget.document.count!, null, growable: true);
+    _images = List<Image?>.filled(widget.document.count!, null, growable: true);
     if (widget.preload) {
       Future.delayed(Duration.zero, () async {
         await widget.document.preloadImages();
-        widget._images = widget.document.images;
+        _images = widget.document.images;
         _preloaded = true;
         if (mounted) setState(() {});
       });
@@ -41,32 +48,46 @@ class _PDFListViewerState extends State<PDFListViewer> {
 
   @override
   void dispose() {
-    for (var i = 0; i < widget._images!.length; i++) {
-      widget._images![i]!.image.evict();
-      widget._images![i] = null;
+    for (var i = 0; i < _images!.length; i++) {
+      _images![i]!.image.evict();
+      _images![i] = null;
     }
-    widget._images = null;
+    _images = null;
     widget.document.clearImageCache();
     widget.document.clearFileCache();
     super.dispose();
   }
 
   Future<Image?>? _loadPage(int index) async {
-    if (index < 0 || index >= widget._images!.length) return null;
-    if (widget._images![index] != null) return widget._images![index];
+    if (index < 0 || index >= _images!.length) return null;
+    if (_images![index] != null) return _images![index];
     final data = await widget.document.getImage(page: index + 1);
-    widget._images![index] = data;
-    return widget._images![index];
+    _images![index] = data;
+    return _images![index];
   }
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+        decoration: null,
+        child: CustomZoomableWidget(
+          onZoomChanged: widget.onZoomChanged,
+          zoomSteps: widget.zoomSteps ?? 3,
+          minScale: widget.minScale ?? 1.0,
+          panLimit: widget.panLimit ?? 1.0,
+          maxScale: widget.maxScale ?? 5.0,
+          autoCenter: true,
+          child: _getViewer(),
+        ));
+  }
+
+  Widget _getViewer() {
     return widget.preload
         ? _preloaded
             ? ListView.builder(
                 itemCount: widget.document.count,
                 itemBuilder: (context, index) {
-                  return widget._images![index]!;
+                  return _images![index]!;
                 })
             : Center(
                 child: CircularProgressIndicator(),
@@ -80,7 +101,7 @@ class _PDFListViewerState extends State<PDFListViewer> {
                 return snapShot.data as Image;
               } else {
                 return Container(
-                  height: widget._loadingPageHeight,
+                  height: widget.loadingPageHeight,
                   width: double.maxFinite,
                   child: Center(child: CircularProgressIndicator()),
                 );
