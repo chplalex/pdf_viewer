760a7ff42db3acfeedb5eb4710bcd02ce747333c
Umesh <umesh.chakradhar@8squarei.com>
2024-04-13 14:29:40 +0545

    fix : dart static analysis fixed

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:advance_pdf_viewer_example/main.dart';
+import 'package:chakre_pdf_viewer_example/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
